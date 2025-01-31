ec107779a8b29868c967c0ca529ec4073fd0b41b
KevinLopezArana <97923461+KevinLopezArana@users.noreply.github.com>
2025-01-31 07:26:53 -0600

    Added an enemy spawn manager and script to handle spawning around the player and I also implemented a 3D asset for our enemies.

---
+++ Assets/Code/Enemy/EnemySpawner.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+    public Transform player;
+
+    public float spawnRadius = 20f;
+    public int maxEnemies = 10;
+    public float spawnInterval = 5f;
+    private int currentEnemyCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+            if(currentEnemyCount < maxEnemies)
+            {
+                SpawnEnemy();
+            }
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Vector3 spawnPosition = GetRandomSpawnPosition();
+        if(spawnPosition != Vector3.zero)
+        {
+            Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+            currentEnemyCount++;
+        }
+    }
+
+
+    Vector3 GetRandomSpawnPosition()
+    {
+        for (int i=0; i < 10; i++)
+        {
+            Vector3 randomDirection = Random.insideUnitSphere * spawnRadius;
+            randomDirection.y = 0f;
+            Vector3 spawnPoint = player.position + randomDirection;
+
+            NavMeshHit hit;
+            if(NavMesh.SamplePosition(spawnPoint, out hit, 5f, NavMesh.AllAreas))
+            {
+                return hit.position;
+            }
+        }
+        return Vector3.zero;
+    }
+
+    
+}
